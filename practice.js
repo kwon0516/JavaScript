6a069d4b34c0f08326ae6f8d2d5bdd02578f9ccd
TackYoung <tackyoung.kwon@imts.co.kr>
2023-05-19 17:48:31 +0900

    [ js ] 심심해서 스플릿, 리플레이스 만들어봄...

--- practice.js
+++ practice.js
@@ -1,17 +1,37 @@
 'use strict';
 
-for (let i = 0; i <= 10; i++)
-{
-    if (i % 2 == 0)
-        console.log(i);
+const split = function(str, sep) {
+    const strLength = str.length;
+    let strResult = "";
+
+    for (let i = 0; i != strLength; i++) {
+        if (str[i] !== sep)
+        strResult += str[i];
+    }
+
+    return strResult;
 }
 
-console.log('------------');
+const replace = function(str, before, after) {
+    const strLength = str.length;
+    let strResult = "";
 
-for (let i = 0; i != 10; i ++)
-{
-    if (i == 8)
-        break;
-        
-    console.log(i);
+    for (let i = 0; i != strLength; i++) {
+        if (str[i] !== before) {
+            strResult += str[i]
+        }
+        else if (str[i] === before) {
+            strResult += after;
+        }
+    }
+    
+    return strResult;
 }
+
+let splitWord = split("H,EL,LO WOR,L,D!", ",");
+
+console.log(splitWord);
+
+let replaceWord = replace("H,EL,LO WOR,L,D!", ",", "!");
+
+console.log(replaceWord);
